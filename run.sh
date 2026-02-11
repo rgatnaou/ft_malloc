b3a72108e789fafc78de5fc335fb77d697628228
rgatnaou <gatnaouiradwane@gmail.com>
2026-02-11 20:03:29 +0100

    add test

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=.
 export LD_PRELOAD=./libft_malloc.so
-export Malloc42Debug=0
+# export Malloc42Debug=0
 "$@"
--- test/correction/test2.c
+++ test/correction/test2.c
@@ -6,25 +6,70 @@
 /*   By: rgatnaou <rgatnaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/21 15:16:57 by jterrazz          #+#    #+#             */
-/*   Updated: 2026/02/04 20:12:54 by rgatnaou         ###   ########.fr       */
+/*   Updated: 2026/02/11 16:26:30 by rgatnaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/malloc.h"
+#include <stdio.h>
+// #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
 
-int	main(void)
+#define MAX_PTRS 10000
+#define MAX_SIZE 4096
+#define ALIGNMENT 16
+
+int main(void)
 {
-	int		i;
-	char	*addr;
+    void *ptrs[MAX_PTRS] = {0};
+    size_t sizes[MAX_PTRS] = {0};
+    int i;
+    int index;
 
-	i = 0;
-	while (i < 1024)
-	{
-		addr = (char*)malloc(1024);
-		addr[0] = 42;
-		free(addr);
-		i++;
-	}
-	// show_alloc_mem();
-	return (0);
+    srand(time(NULL));
+
+    while (2) // infinite stress loop
+    {
+        /* Random allocations */
+        // printf("\n******start :\n");
+        for (i = 0; i < MAX_PTRS; i++)
+        {
+            // printf("start loop: i=%d\n",i);
+            sizes[i] = rand() % MAX_SIZE + 1;
+            // printf("allocation size: %ld",sizes[i]);
+            ptrs[i] = malloc(sizes[i]);
+
+            if (!ptrs[i])
+            {
+                printf("malloc failed\n");
+                exit(1);
+            }
+
+            /* Alignment check */
+            if ((uintptr_t)ptrs[i] % ALIGNMENT != 0)
+            {
+                printf("❌ Alignment broken at %p\n", ptrs[i]);
+                abort();
+            }
+
+            /* Fill full block (detect overflow issues) */
+            memset(ptrs[i], 0xAA, sizes[i]);
+        }
+		printf("✅ Allocated %d blocks\n", MAX_PTRS);
+        /* Random free order */
+        for (i = 0; i < MAX_PTRS; i++)
+        {
+            index = rand() % MAX_PTRS;
+            if (ptrs[index])
+            {
+                free(ptrs[index]);
+                ptrs[index] = NULL;
+            }
+        }
+		printf("✅ Freed blocks in random order\n");
+    }
+
+    return 0;
 }
